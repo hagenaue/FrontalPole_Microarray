8134787fad4b1bb474d1978461f7412f1f99b4fe
liamctf <liam.c.thew.forrester@gmail.com>
2021-05-28 18:20:25 -0400

    Analyses of Iwamoto and Maycox data, respectively.

--- Maycox_Analysis.R
+++ Maycox_Analysis.R
@@ -1,66 +1,96 @@
-#Iwamoto Analysis Code
+#Maycox Analysis Code
 #Liam
 
-setwd("C:/Users/Frosty/Desktop/Research/Summer 2021 Frontal Pole Research/FrontalPole_Microarray/Iwamoto_GSE12654")
+# TO-DO:
+# 1) Diagnosis is not included in characteristics;I see it on GSE tho, so I need to either manually input it or write up some code to extract it
+
+setwd("C:/Users/Frosty/Desktop/Research/Summer 2021 Frontal Pole Research/FrontalPole_Microarray/Maycox_GSE17612")
 
 library(GEOquery)
-gse <- getGEO("GSE12654", GSEMatrix = FALSE)
+gse <- getGEO("GSE17612", GSEMatrix = FALSE)
 
 head(Meta(gse))
 str(Meta(gse))
-Meta(GSMList(gse)$GSM317649)$characteristics_ch1
-# [1] "prefrontal cortex (BA10)" "control"
+Meta(GSMList(gse)$GSM439778)$characteristics_ch1
+# [1] "gender: Male"            "age: 74"                 "post-mortem delay: 4.5h"
+# [4] "ph: 6"  
 
+# Diagnosis is not included in characteristics, so I extract it later on using the samples' titles
+Meta(GSMList(gse)$GSM439778)$title
+# [1] "S014_Scz_M_74"
 
-sub("tissue: ","", Meta(GSMList(gse)$GSM317649)$characteristics_ch1[1])
-sub("diagnosis: ","", Meta(GSMList(gse)$GSM317649)$characteristics_ch1[2], fixed = T)
+sub("gender: ","", Meta(GSMList(gse)$GSM439778)$characteristics_ch1[1])
+as.numeric(sub("age: ","", Meta(GSMList(gse)$GSM439778)$characteristics_ch1[2]))
+PMDlabelremoved<-sub("post-mortem delay: ","", Meta(GSMList(gse)$GSM439778)$characteristics_ch1[3], fixed = T)
+as.numeric(substr(PMDlabelremoved, 1, nchar(PMDlabelremoved)-1))
+rm(PMDlabelremoved)
+as.numeric(sub("ph: ","", Meta(GSMList(gse)$GSM439778)$characteristics_ch1[4]))
 
 SampleID<-as.matrix(names(GSMList(gse)))
-Tissue<-matrix("a", nrow=50, ncol=1)
-Diagnosis2<-matrix("a", nrow=50, ncol=1)
+Gender<-matrix("a", nrow=51, ncol=1)
+Age<-matrix(0, nrow=51, ncol=1)
+BrainpH<-matrix(0, nrow=51, ncol=1)
+PMD<-matrix(0, nrow=51, ncol=1)
+Diagnosis2<-matrix("a", nrow=51, ncol=1)
 
 
 i<-1
-for(i in c(1:50)){
-  Tissue[i]<-sub("tissue: ","", Meta(GSMList(gse)[[i]])$characteristics_ch1[1])
-  Diagnosis2[i]<-sub("disease state: ","", Meta(GSMList(gse)[[i]])$characteristics_ch1[2], fixed=T)
+for(i in c(1:51)){
+  Gender[i]<-sub("gender: ","", Meta(GSMList(gse)[[i]])$characteristics_ch1[1])
+  Age[i]<-as.numeric(sub("age: ","", Meta(GSMList(gse)[[i]])$characteristics_ch1[2]))
+  PMDlabelremoved<-sub("post-mortem delay: ","", Meta(GSMList(gse)[[i]])$characteristics_ch1[3], fixed = T)
+  PMD[i]<-as.numeric(substr(PMDlabelremoved, 1, nchar(PMDlabelremoved)-1))
+  rm(PMDlabelremoved)
+  BrainpH[i]<-as.numeric(sub("ph: ","", Meta(GSMList(gse)[[i]])$characteristics_ch1[4]))
+  if(all(substr(Meta(GSMList(gse)[[i]])$title, 6,8)=="Scz")) Diagnosis2[i]<-"Scz" else Diagnosis2[i]<-"Control"
 }
 
+# This entry does not have a gender listed, so the data needs to be assigned outside of the for loop
+Gender[11]<-NA
+Age[11]<-as.numeric(sub("age: ","", Meta(GSMList(gse)$GSM439788)$characteristics_ch1[1]))
+PMDlabelremoved<-sub("post-mortem delay: ","", Meta(GSMList(gse)$GSM439788)$characteristics_ch1[2], fixed = T)
+PMD[11]<-as.numeric(substr(PMDlabelremoved, 1, nchar(PMDlabelremoved)-1))
+rm(PMDlabelremoved)
+BrainpH[11]<-as.numeric(sub("ph: ","", Meta(GSMList(gse)$GSM439788)$characteristics_ch1[3]))
+
 Diagnosis<-Diagnosis2
-Diagnosis<-relevel(as.factor(Diagnosis), ref="control")
+Diagnosis<-relevel(as.factor(Diagnosis), ref="Control")
 
-Iwamoto_SampleCharacteristics<-data.frame(SampleID, Tissue, Diagnosis, stringsAsFactors=F)
+Maycox_SampleCharacteristics<-data.frame(SampleID, Gender, Age, PMD, BrainpH, Diagnosis, stringsAsFactors=F)
 
-head(Iwamoto_SampleCharacteristics)
+head(Maycox_SampleCharacteristics)
 
-write.csv(Iwamoto_SampleCharacteristics, "Iwamoto_SampleCharacteristics.csv")
+write.csv(Maycox_SampleCharacteristics, "Maycox_SampleCharacteristics.csv")
 
 library(org.Hs.eg.db)
 library(plyr)
 library(affy)
 
-#CDF and the chip.
-install.packages("C:/Users/Frosty/Desktop/Research/Summer 2021 Frontal Pole Research/FrontalPole_Microarray/pd.hgu95av2.hs.entrezg_25.0.0.tar.gz", repos = NULL, type = "source")
-install.packages("C:/Users/Frosty/Desktop/Research/Summer 2021 Frontal Pole Research/FrontalPole_Microarray/hgu95av2hsentrezgprobe_25.0.0.tar.gz", repos = NULL, type = "source")
-install.packages("C:/Users/Frosty/Desktop/Research/Summer 2021 Frontal Pole Research/FrontalPole_Microarray/hgu95av2hsentrezgcdf_25.0.0.tar.gz", repos = NULL, type = "source")
-
-#Reads .CEL files into an Affybatch
-data2<-ReadAffy(cdfname ="hgu95av2hsentrezg")
+# CDF and the chip.
+install.packages("C:/Users/Frosty/Desktop/Research/Summer 2021 Frontal Pole Research/FrontalPole_Microarray/Maycox_GSE17612/pd.hgu133plus2.hs.entrezg_25.0.0.tar.gz", repos = NULL, type = "source")
+install.packages("C:/Users/Frosty/Desktop/Research/Summer 2021 Frontal Pole Research/FrontalPole_Microarray/Maycox_GSE17612/hgu133plus2hsentrezgprobe_25.0.0.tar.gz", repos = NULL, type = "source")
+install.packages("C:/Users/Frosty/Desktop/Research/Summer 2021 Frontal Pole Research/FrontalPole_Microarray/Maycox_GSE17612/hgu133plus2hsentrezgcdf_25.0.0.tar.gz", repos = NULL, type = "source")
+# Reads .CEL files into an Affybatch
+data2<-ReadAffy(cdfname ="hgu133plus2hsentrezg")
 str(data2)
 data2
+# AffyBatch object
+# size of arrays=1164x1164 features (40 kb)
+# cdf=hgu133plus2hsentrez (??? affyids)
+# number of samples=51
 
 ScanDate2<-protocolData(data2)$ScanDate
 ScanDateDayOnly2<-matrix("a", nrow=50, ncol=1)
 
 i<-1
-for(i in c(1:50)){
+for(i in c(1:51)){
   ScanDateDayOnly2[i]<-substr(ScanDate2[i], 1, 8)
 }
 
 ScanDate<-ScanDate2
-ScanDate<-relevel(as.factor(ScanDate2), ref= "08/24/01 15:28:22")
+ScanDate<-relevel(as.factor(ScanDate2), ref= "01/21/04 10:44:01")
 ScanDateDayOnly<-ScanDateDayOnly2
-ScanDateDayOnly<-relevel(as.factor(ScanDateDayOnly), ref="08/24/01")
+ScanDateDayOnly<-relevel(as.factor(ScanDateDayOnly), ref="01/21/04")
 
 #Converts the data2 AffyBatch into an ExpressionSet object using the robust multi-array average (RMA) expression measure. 
 #The expression measure is given in log base 2 scale.
@@ -70,7 +100,6 @@ RMAExpression_customCDFplus2<-read.delim("data_customCDFplus2.txt", sep="\t")
 str(RMAExpression_customCDFplus2)
 #'data.frame':	8551 obs. of  51 variables
 write.csv(RMAExpression_customCDFplus2, "RMAExpression_customCDFplus2.csv")
-
 
 BiocManager::install("AffyRNADegradation")
 library(AffyRNADegradation)
@@ -109,15 +138,15 @@ write.csv(RMAExpression_customCDFAnnotation2plus2, "RMAExpression_customCDFAnnot
 
 SignalSortedNoNA3<-as.matrix(RMAExpression_customCDFplus2[,-1])
 
-setwd("C:/Users/Frosty/Desktop/Research/Summer 2021 Frontal Pole Research/FrontalPole_Microarray/Iwamoto_GSE12654/Graphs")
+setwd("C:/Users/Frosty/Desktop/Research/Summer 2021 Frontal Pole Research/FrontalPole_Microarray/Maycox_GSE17612/Graphs")
 
 
 #Variable Analysis
-SubjectFactorVariables<-cbind(Diagnosis, ScanDateDayOnly)
-colnames(SubjectFactorVariables)<-c("Diagnosis", "ScanDateDayOnly")
+SubjectFactorVariables<-cbind(Diagnosis, Gender, ScanDateDayOnly)
+colnames(SubjectFactorVariables)<-c("Diagnosis", "Gender", "ScanDateDayOnly")
 
-SubjectContinuousVariables<-cbind(RNADegradPerSample)
-colnames(SubjectContinuousVariables)<-c("RNADegradPerSample")
+SubjectContinuousVariables<-cbind(BrainpH, PMD, Age, RNADegradPerSample)
+colnames(SubjectContinuousVariables)<-c("BrainpH", "PMD", "Age", "RNADegradPerSample")
 
 for (i in 1:length(SubjectContinuousVariables[1,])){
   png(paste(paste("Histogram of", colnames(SubjectContinuousVariables)[i], sep="  "), "png", sep="."))	
@@ -148,11 +177,12 @@ for (i in 1:length(SubjectContinuousVariables[1,])){
 }
 
 #Creating a text file of contingency tables to visually examine the relationships between categorical subject variables:
-CrossTabsIV<-file("14 Cross Tabs Between Subject Factors.txt")
+CrossTabsIV<-file("Cross Tabs Between Subject Factors.txt")
 out<-c(
   capture.output(
     
     summary(Diagnosis),
+    summary(Gender),
     summary(ScanDateDayOnly),
     
     for (i in 1:length(SubjectFactorVariables[1,])){
@@ -165,18 +195,18 @@ out<-c(
     }
   )
 )
-cat(out, file="14 Cross Tabs Between Subject Factors.txt", sep="\n", append=TRUE)
+cat(out, file="Cross Tabs Between Subject Factors.txt", sep="\n", append=TRUE)
 close(CrossTabsIV)
 rm(out)
 
 library(car)
 
-StatisticalRelationshipsIV<-file("14 Statistical Relationships between Subject Variables.txt")
+StatisticalRelationshipsIV<-file("Statistical Relationships between Subject Variables.txt")
 out<-c(
   
   capture.output(
     #Calculating the variance inflation factor (vif) to determine which subject variables are highly related to other subject variables in the data set. Most important, of course, is whether any of the subject variables strongly correlate with Diagnosis. 
-    vif(lm(SignalSortedNoNA3[1,]~Diagnosis+RNADegradPerSample+ScanDateDayOnly))
+    vif(lm(SignalSortedNoNA3[1,]~BrainpH+PMD+Diagnosis+Gender+Age+RNADegradPerSample+ScanDateDayOnly))
     
   ),
   
@@ -213,12 +243,12 @@ out<-c(
   )
   
 )
-cat(out, file="14 Statistical Relationships between Subject Variables.txt", sep="\n", append=TRUE)
+cat(out, file="Statistical Relationships between Subject Variables.txt", sep="\n", append=TRUE)
 close(StatisticalRelationshipsIV)
 rm(out)
 
 #Flagging variables that are collinear with other subject variables:
-FlaggedRelationshipsBetweenIV<-file("14 Flagged Relationships Between Subject Variables.txt")
+FlaggedRelationshipsBetweenIV<-file("Flagged Relationships Between Subject Variables.txt")
 out<-c(
   
   #Using linear regression to examine the statistical relationships between the continuous subject variables:
@@ -254,6 +284,6 @@ out<-c(
     }
   )
 )
-cat(out, file="14 Flagged Relationships Between Subject Variables.txt", sep="\n", append=TRUE)
+cat(out, file="Flagged Relationships Between Subject Variables.txt", sep="\n", append=TRUE)
 close(FlaggedRelationshipsBetweenIV)
 rm(out)
